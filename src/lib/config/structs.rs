47ad35d92e2d49b774b6cc368719e32cb7649066
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 16:25:05 +0100

    ✨ Add backuponly option

--- src/lib/config/structs.rs
+++ src/lib/config/structs.rs
@@ -6,7 +6,9 @@ pub struct FileInfo {
     /// The path to the file / directory (relative to /home/$USER)ex: .config/i3/config
     pub path: String,
     /// Only install if distro is ? 
-    pub onlyon: Option<String>
+    pub onlyon: Option<String>,
+    /// If the file shouldn't be installed, mark is as "backuponly"
+    pub backuponly: Option<bool>,
 }
 #[derive(Debug, Serialize, Deserialize)] // Required by serde_yaml
 pub struct KelpConfig {
