c2bd0ffe5813bf921b8196fc91be8513f79fd173
Woomymy <github@woomy.ovh>
2021-01-13 18:54:10 +0100

    ✨ Add autoconfig fonction

--- src/lib/config/mod.rs
+++ src/lib/config/mod.rs
@@ -2,6 +2,8 @@ mod loader;
 pub use loader::*;
 mod structs;
 pub use structs::*;
+mod autoconfig;
+pub use autoconfig::*;
 // Use cli::* to make sure imports not look like this:
 // lib::cli::cli::Cli;
 // Now it's more like this:
