c1196f9007705a33b6c377fbe39e43130fcaedd1
Woomy <woomy@woomy.be>
2021-01-13 18:54:10 +0100

    ✨ Add autoconfig fonction

---
+++ src/lib/config/autoconfig.rs
@@ -0,0 +1,12 @@
+use super::structs::{KelpConfig, FileInfo};
+/// Find files that can automaticaly copyied by Kelp
+pub fn autoconfig(config: &mut KelpConfig) -> anyhow::Result<()> {
+    let rawhome = include_str!("../../config/home.yaml");
+    let home: Vec<FileInfo> = serde_yaml::from_str(&rawhome)?;
+    for f in home {
+        if std::path::Path::new(&format!("{}/{}", std::env::var("HOME").unwrap_or_else(|_| String::from(".")), f.path)).exists() {
+            config.homedir.push(f);
+        }
+    }
+    Ok(())
+}
