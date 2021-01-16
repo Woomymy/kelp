b04a8d5b66468a7c9d819699dcc79a91b25930e2
Woomy4680-exe <matzar1808@gmail.com>
2021-01-16 18:45:06 +0100

    🎨 Format

--- src/lib/config/autoconfig.rs
+++ src/lib/config/autoconfig.rs
@@ -1,10 +1,16 @@
-use super::structs::{KelpConfig, FileInfo};
+use super::structs::{FileInfo, KelpConfig};
 /// Find files that can automaticaly copyied by Kelp
 pub fn autoconfig(config: &mut KelpConfig) -> anyhow::Result<()> {
     let rawhome = include_str!("../../config/home.yaml");
     let home: Vec<FileInfo> = serde_yaml::from_str(&rawhome)?;
     for f in home {
-        if std::path::Path::new(&format!("{}/{}", std::env::var("HOME").unwrap_or_else(|_| String::from(".")), f.path)).exists() {
+        if std::path::Path::new(&format!(
+            "{}/{}",
+            std::env::var("HOME").unwrap_or_else(|_| String::from(".")),
+            f.path
+        ))
+        .exists()
+        {
             config.homedir.push(f);
         }
     }
