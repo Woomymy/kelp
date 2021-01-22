e896b4ec3f0ada9d734dd989582b846f37fac117
Woomy <woomy@woomy.be>
2021-01-22 11:26:06 +0100

    ♻️ Readd CLI Options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+mod lib;
+use lib::cli::Cli;
 fn main() {
-    println!("Hello, world");
+    match Cli::from_args() {
+        Cli::Save { } => {
+            println!("Not yet implemented!");
+        }
+        Cli::Install { } => {
+            println!("Not yet implemented!");
+        }
+        Cli::Init { } => {
+            println!("Not yet implemented!");
+        }
+    }
 }
