586df69849c445423cb446bc1bf34659b6d6c72e
Woomy4680-exe <matzar1808@gmail.com>
2021-02-04 14:42:50 +0100

    ✅ Start working on tests for bundled yaml

--- src/lib/cli/save.rs
+++ src/lib/cli/save.rs
@@ -1,11 +1,12 @@
 use crate::lib::{
     fsutil::paths::get_root, terminal::colors::*, terminal::debug::debug_print, util::env::is_debug,
 };
+/// Backup dotfiles
 pub fn save() -> anyhow::Result<()> {
     let root = get_root()?;
     cyan(&format!("[INFO] Saving dotfiles {}...", root));
     debug_print("Building OS list...");
-    let oses = crate::lib::util::os::build_os_list()?;
+    let oses = crate::lib::util::os::build_os_list()?; // Get a Vec<Os> wich is a list of reconisables oses
     if is_debug() {
         debug_print("Oses:");
         for os in oses {
