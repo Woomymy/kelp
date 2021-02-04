2096885aff9c199c37f3d7615c62f58b8be94d11
Woomy4680-exe <matzar1808@gmail.com>
2021-02-04 17:16:59 +0100

    🎨 Format

--- tests/fs.rs
+++ tests/fs.rs
@@ -2,5 +2,8 @@ extern crate libkelp;
 #[test]
 pub fn external_root_works() {
     std::env::set_var("DOTFILES_ROOT", "/");
-    assert_eq!(String::from("/"), libkelp::lib::fsutil::paths::get_root().unwrap());
+    assert_eq!(
+        String::from("/"),
+        libkelp::lib::fsutil::paths::get_root().unwrap()
+    );
 }
