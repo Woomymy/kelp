389735f5be99d896c1de95b99549b89ebbf08462
Woomy4680-exe <matzar1808@gmail.com>
2021-02-04 10:05:21 +0100

    ✨ Add debug print

--- src/lib/cli/save.rs
+++ src/lib/cli/save.rs
@@ -1,8 +1,10 @@
 use crate::lib::fsutil::paths::get_root;
 use crate::lib::terminal::colors::*;
+use crate::lib::terminal::debug::debug_print;
 pub fn save() -> anyhow::Result<()> {
     let root = get_root()?;
     cyan(&format!("[INFO] Saving dotfiles {}...", root));
+    debug_print("Building OS list...");
     crate::lib::util::os::build_os_list(root)?;
     Ok(())
 }
--- src/lib/util/os.rs
+++ src/lib/util/os.rs
@@ -2,14 +2,11 @@ pub struct Os {
     /// Name of the os
     pub name: String,
     /// File used to reconise it
-    pub file: String
+    pub file: String,
 }
 /// Find oses to detect
 pub fn build_os_list(root: String) -> anyhow::Result<Vec<Os>> {
-    let search_paths = vec!(
-        "/etc/kelp/os.yaml",
-        "/etc/kelp/os.yml"
-    );
-    
-    Ok(vec!())
+    let search_paths = vec!["/etc/kelp/os.yaml", "/etc/kelp/os.yml"];
+
+    Ok(vec![])
 }
