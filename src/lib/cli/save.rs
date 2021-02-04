d432ffcaa6f3d19ecda6e7fcac9b025b975d5b83
Woomy <woomy@woomy.be>
2021-02-04 09:08:38 +0100

    ✨ Add some infos

--- src/lib/cli/save.rs
+++ src/lib/cli/save.rs
@@ -1,6 +1,8 @@
 use crate::lib::fsutil::paths::get_root;
+use crate::lib::terminal::colors::*;
 pub fn save() -> anyhow::Result<()> {
     let root = get_root()?;
-    println!("{}", root);
+    cyan(&format!("[INFO] Saving dotfiles {}...", root));
+    crate::lib::util::os::build_os_list(root)?;
     Ok(())
 }
