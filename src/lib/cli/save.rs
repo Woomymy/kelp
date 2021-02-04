101490635d85a7dd39040da7005ad5aa4d229ff8
Woomy4680-exe <matzar1808@gmail.com>
2021-02-04 12:19:04 +0100

    🎨 Format

--- src/lib/cli/save.rs
+++ src/lib/cli/save.rs
@@ -1,6 +1,4 @@
-use crate::lib::fsutil::paths::get_root;
-use crate::lib::terminal::colors::*;
-use crate::lib::terminal::debug::debug_print;
+use crate::lib::{fsutil::paths::get_root, terminal::colors::*, terminal::debug::debug_print};
 pub fn save() -> anyhow::Result<()> {
     let root = get_root()?;
     cyan(&format!("[INFO] Saving dotfiles {}...", root));
