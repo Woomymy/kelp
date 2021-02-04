7979e64af445ffb14f8e90101d945db6c6ac7b1c
Woomy <woomy@woomy.be>
2021-02-04 13:17:43 +0100

    ⚡️ Optimise os priting (don't iterate if debug mode is disabled)

--- src/lib/cli/save.rs
+++ src/lib/cli/save.rs
@@ -1,12 +1,16 @@
-use crate::lib::{fsutil::paths::get_root, terminal::colors::*, terminal::debug::debug_print};
+use crate::lib::{
+    fsutil::paths::get_root, terminal::colors::*, terminal::debug::debug_print, util::env::is_debug,
+};
 pub fn save() -> anyhow::Result<()> {
     let root = get_root()?;
     cyan(&format!("[INFO] Saving dotfiles {}...", root));
     debug_print("Building OS list...");
     let oses = crate::lib::util::os::build_os_list(root)?;
-    debug_print("Oses:");
-    for os in oses {
-        debug_print(&format!("Name: {} | File: {}", os.name, os.file));
+    if is_debug() {
+        debug_print("Oses:");
+        for os in oses {
+            debug_print(&format!("Name: {} | File: {}", os.name, os.file));
+        }
     }
     Ok(())
 }
