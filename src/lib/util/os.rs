8b94940a006468b28267a9e9ca981215f18756fe
Woomy <woomy@woomy.be>
2021-02-04 12:42:35 +0100

    ✨ Finish build_os_list function

--- src/lib/util/os.rs
+++ src/lib/util/os.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+use serde::{Deserialize, Serialize};
+#[derive(Debug,Serialize,Deserialize)]
 pub struct Os {
     /// Name of the os
     pub name: String,
@@ -6,7 +9,21 @@ pub struct Os {
 }
 /// Find oses to detect
 pub fn build_os_list(root: String) -> anyhow::Result<Vec<Os>> {
-    let search_paths = vec!["/etc/kelp/os.yaml", "/etc/kelp/os.yml"];
-
-    Ok(vec![])
+    let mut osyaml: Vec<Os> = vec!();
+    let search_paths = vec!["/etc/kelpdot/os.yaml", "/etc/kelpdot/os.yml"];
+    for path in search_paths {
+        if Path::new(path).exists() {
+            let content = std::fs::read_to_string(path)?;
+            let yaml: Vec<Os> = serde_yaml::from_str(&content)?;
+            for os in yaml {
+                osyaml.push(os);
+            }
+        }
+    }
+    let bundled = include_str!("../../config/oses.yaml");
+    let oses: Vec<Os> = serde_yaml::from_str(&bundled)?;
+    for os in oses {
+        osyaml.push(os);
+    }
+    Ok(osyaml)
 }
