aa8719b97469f20c39920e1878bc7abf43c8f65f
Woomy <woomy@woomy.be>
2021-02-05 15:24:52 +0100

    🥅 Add paths resolution

--- src/lib/cli/migrate.rs
+++ src/lib/cli/migrate.rs
@@ -1,12 +1,8 @@
-use std::path::Path;
 use crate::lib::{
-    structs::{
-        legacy::LegacyKelpConfig
-    },
-    fsutil::paths::get_root,
+    config::migration::migrate_configs, fsutil::paths::get_root, structs::legacy::LegacyKelpConfig,
     terminal::colors::*,
-    config::migration::migrate_configs,
 };
+use std::path::Path;
 pub fn migrate() -> anyhow::Result<()> {
     let root = get_root()?;
     cyan(&format!("[INFO] Migrating dotfiles {}", root));
@@ -14,7 +10,8 @@ pub fn migrate() -> anyhow::Result<()> {
         red(&format!("[ERROR] {}/kelp.yaml doesn't exist!", root));
         std::process::exit(1);
     }
-    let config: LegacyKelpConfig = serde_yaml::from_str(&std::fs::read_to_string(format!("{}/kelp.yaml",root))?)?;
+    let config: LegacyKelpConfig =
+        serde_yaml::from_str(&std::fs::read_to_string(format!("{}/kelp.yaml", root))?)?;
     let new = migrate_configs(config)?;
     std::fs::write(format!("{}/kelp.yaml", root), serde_yaml::to_string(&new)?)?;
     Ok(())
--- src/lib/cli/save.rs
+++ src/lib/cli/save.rs
@@ -1,10 +1,11 @@
 use crate::lib::{
-    fsutil::paths::get_root,
+    config::loader::load_cfg,
+    fsutil::paths::{get_root, get_to_make},
     terminal::colors::*,
     terminal::debug::debug_print,
     util::os::get_host_os,
-    config::loader::load_cfg,
 };
+use std::path::Path;
 /// Backup dotfiles
 pub fn save() -> anyhow::Result<()> {
     let root = get_root()?;
@@ -12,10 +13,33 @@ pub fn save() -> anyhow::Result<()> {
     debug_print("Building OS list...");
     let os = get_host_os()?;
     cyan(&format!("[INFO] Found Os {}", os.prettyname));
-    let config = load_cfg(root)?;
+    let config = load_cfg(root.clone())?;
     if let Some(files) = config.homefiles {
+        let home = std::env::var("HOME")?;
+        debug_print(&format!("Home: {}", home));
+        if Path::new(&format!("{}/home", root)).exists() {
+            std::fs::remove_dir_all(&format!("{}/home", root))?;
+        }
+        std::fs::create_dir(format!("{}/home", root))?;
         for f in files {
             green(&format!("[SAVE] Copying file {}...", f));
+            let path = format!("{}/{}", home, f.path);
+            let file = Path::new(&path);
+            if !file.exists() {
+                magenta(&format!(
+                    "[WARNING] Skipping {} because it doesn't exist!",
+                    f
+                ));
+                break;
+            }
+            let tomake = get_to_make(f.path)?;
+        }
+    }
+    if let Some(files) = config.rootfiles {
+        for f in files {
+            // WIP
+            green(&format!("[SAVE] Copying file {}", f));
+            //let tomake = get_to_make(f.path)?;
         }
     }
     Ok(())
