b543333b94156afcae03879ee96cc541dd8fb58d
Woomymy <github@woomy.ovh>
2021-02-05 17:13:25 +0100

    ✨ Add shadow-rust build info

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
+#[macro_use]
+extern crate shadow_rs;
+shadow!(build);
 use structopt::StructOpt;
 mod lib;
-use lib::{cli::opts::Cli, terminal};
+use lib::{cli::opts::Cli, terminal::{messages::not_yet_implemented, debug::debug_print, colors::*}};
 fn main() -> anyhow::Result<()> {
+    green(&format!("KelpDot V{}-{}", build::PKG_VERSION, build::BRANCH));
+    debug_print(&format!("Commit {} Branch {}", build::SHORT_COMMIT, build::BRANCH));
+    debug_print(&format!("Built with {}-{}", build::RUST_VERSION, build::RUST_CHANNEL));
+    debug_print(&format!("Commit by {}", build::COMMIT_AUTHOR));
+    green("==============");
     // Check CLI options
     match Cli::from_args() {
         Cli::Save {} => {
             lib::cli::save::save()?;
         }
         Cli::Install {} => {
-            terminal::messages::not_yet_implemented();
+            not_yet_implemented();
         }
         Cli::Init {} => {
-            terminal::messages::not_yet_implemented();
+            not_yet_implemented();
         }
         Cli::Migrate {} => {
             lib::cli::migrate::migrate()?;
