a90e20459b13dd5da456a9098609a2e52b5615d9
Woomymy <github@woomy.ovh>
2021-02-05 13:48:57 +0100

    🚧 Add config loader

--- src/lib/config/loader.rs
+++ src/lib/config/loader.rs
@@ -1,1 +1,17 @@
-
+use std::path::Path;
+use crate::lib::{
+    structs::config::KelpDotConfig,
+    terminal::{
+        debug::debug_print,
+        colors::{red},
+    },
+};
+pub fn load_cfg(root: String) ->  anyhow::Result<KelpDotConfig> {
+    if !Path::new(&format!("{}/kelp.yaml", root)).exists() {
+        red(&format!("File {}/kelp.yaml not found!", root));
+        std::process::exit(1);
+    }
+    debug_print(&format!("Loading config {}/kelp.yaml", root));
+    let cfg: KelpDotConfig = serde_yaml::from_str(&std::fs::read_to_string(format!("{}/kelp.yaml", root))?)?;
+    Ok(cfg)
+}
