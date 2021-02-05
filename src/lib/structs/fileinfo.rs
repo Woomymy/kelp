86335d18d27c1274348af7fd0d0cba2862a30f66
Woomy <woomy@woomy.be>
2021-02-05 20:38:59 +0100

    ✅ Add migration test without values

--- src/lib/structs/fileinfo.rs
+++ src/lib/structs/fileinfo.rs
@@ -1,5 +1,5 @@
 pub use serde::{Deserialize, Serialize};
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(PartialEq,Serialize, Deserialize, Debug, Clone)]
 /// FileINFO struct: used for represent a file to backup
 pub struct FileInfo {
     /// The name of the file, optional but recommended to reconize files
