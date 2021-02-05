672378ca6f5a675f1e015789e77c8806cf56b0fd
Woomy4680-exe <matzar1808@gmail.com>
2021-02-05 17:54:42 +0100

    🎨 Format

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,22 @@ extern crate shadow_rs;
 shadow!(build);
 use structopt::StructOpt;
 mod lib;
-use lib::{cli::opts::Cli, terminal::{messages::not_yet_implemented, debug::debug_print, colors::*}};
+use lib::{
+    cli::opts::Cli,
+    terminal::{colors::*, debug::debug_print, messages::not_yet_implemented},
+};
 fn main() -> anyhow::Result<()> {
     green(&format!("KelpDot V{}", build::PKG_VERSION));
-    debug_print(&format!("Commit {} Branch {}", build::SHORT_COMMIT, build::BRANCH));
-    debug_print(&format!("Built with {}-{}", build::RUST_VERSION, build::RUST_CHANNEL));
+    debug_print(&format!(
+        "Commit {} Branch {}",
+        build::SHORT_COMMIT,
+        build::BRANCH
+    ));
+    debug_print(&format!(
+        "Built with {}-{}",
+        build::RUST_VERSION,
+        build::RUST_CHANNEL
+    ));
     debug_print(&format!("Commit by {}", build::COMMIT_AUTHOR));
     green("==============");
     // Check CLI options
