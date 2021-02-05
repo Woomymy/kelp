ec054977bffb86818918f93da22a7fe97e0ca048
Woomymy <github@woomy.ovh>
2021-02-05 20:39:41 +0100

    ✅ Add Migration tests with values

--- tests/migration_test.rs
+++ tests/migration_test.rs
@@ -15,3 +15,28 @@ pub fn test_config_migration_empty() {
         postrun: Some(vec![])
     });
 }
+#[test]
+pub fn test_config_migration_with_values() {
+    let old = libkelp::lib::structs::legacy::LegacyKelpConfig {
+        name: String::from("Hello"),
+        homedir: vec![legacy::LegacyFileInfo {
+            name: Some(String::from("Hello")),
+            path: String::from("/etc/hellow"),
+            backuponly: Some(true),
+            onlyon: Some(String::from("gentoo")),
+        }],
+        rootfiles: vec![]
+    };
+    assert_eq!(libkelp::lib::config::migration::migrate_configs(old).unwrap(), libkelp::lib::structs::config::KelpDotConfig {
+        homefiles: Some(vec![fileinfo::FileInfo {
+            name: Some(String::from("Hello")),
+            path: String::from("/etc/hellow"),
+            backuponly: Some(true),
+            onlyon: Some(String::from("gentoo")),
+        }]),
+        rootfiles: Some(vec![]),
+        postsave: Some(vec![]),
+        prerun: Some(vec![]),
+        postrun: Some(vec![])
+    });
+}
