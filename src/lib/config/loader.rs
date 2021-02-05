3cd3459e4ba15a48a030bb875f0891b8e3c3c2e8
Woomymy <github@woomy.ovh>
2021-02-05 15:24:52 +0100

    🥅 Add paths resolution

--- src/lib/config/loader.rs
+++ src/lib/config/loader.rs
@@ -1,17 +1,15 @@
-use std::path::Path;
 use crate::lib::{
     structs::config::KelpDotConfig,
-    terminal::{
-        debug::debug_print,
-        colors::{red},
-    },
+    terminal::{colors::red, debug::debug_print},
 };
-pub fn load_cfg(root: String) ->  anyhow::Result<KelpDotConfig> {
+use std::path::Path;
+pub fn load_cfg(root: String) -> anyhow::Result<KelpDotConfig> {
     if !Path::new(&format!("{}/kelp.yaml", root)).exists() {
         red(&format!("File {}/kelp.yaml not found!", root));
         std::process::exit(1);
     }
     debug_print(&format!("Loading config {}/kelp.yaml", root));
-    let cfg: KelpDotConfig = serde_yaml::from_str(&std::fs::read_to_string(format!("{}/kelp.yaml", root))?)?;
+    let cfg: KelpDotConfig =
+        serde_yaml::from_str(&std::fs::read_to_string(format!("{}/kelp.yaml", root))?)?;
     Ok(cfg)
 }
