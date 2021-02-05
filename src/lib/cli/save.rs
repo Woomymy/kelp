e8fe02b3c7a78460de03a1476464bab8ec929f77
Woomymy <github@woomy.ovh>
2021-02-05 17:00:36 +0100

    ✨ Homefiles save working

--- src/lib/cli/save.rs
+++ src/lib/cli/save.rs
@@ -1,6 +1,6 @@
 use crate::lib::{
     config::loader::load_cfg,
-    fsutil::paths::{get_root, get_to_make},
+    fsutil::{copy::copy, paths::{get_root, get_to_make}},
     terminal::colors::*,
     terminal::debug::debug_print,
     util::os::get_host_os,
@@ -14,15 +14,15 @@ pub fn save() -> anyhow::Result<()> {
     let os = get_host_os()?; // Here we get guest os; If OS is unreconized, return a generic GNU / Linux System
     cyan(&format!("[INFO] Found Os {}", os.prettyname));
     let config = load_cfg(root.clone())?; // Load a KelpConfig struct wich is basically $DOTFILES_ROOT/kelp.yaml
-    // If config has "homefiles" keys, copy each $HOME/$FILE
     if let Some(files) = config.homefiles {
+        // If config has "homefiles" keys, copy each $HOME/$FILE
         let home = std::env::var("HOME")?; // Get $HOME path or crash
         debug_print(&format!("Home: {}", home));
-        
+
         // Make sur that $DOTFILES_ROOT/home doesn't exist
         // or doesn't contain files
         if Path::new(&format!("{}/home", root)).exists() {
-            std::fs::remove_dir_all(&format!("{}/home", root))?; 
+            std::fs::remove_dir_all(&format!("{}/home", root))?;
         }
         std::fs::create_dir(format!("{}/home", root))?;
         for f in files {
@@ -38,20 +38,24 @@ pub fn save() -> anyhow::Result<()> {
                 break;
             }
             // Get path to make
-            // Example: 
+            // Example:
             // home/**.config/i3** directory
-            let tomake = get_to_make(f.path)?; 
+            let tomake = get_to_make(f.path)?;
+            // Create the file
+            std::fs::create_dir_all(format!("{}/home/{}", root, tomake))?;
+            copy(path.clone(), format!("{}/home/{}/{}", root, tomake, file.file_name().unwrap().to_str().unwrap().to_owned()))?;
         }
+        cyan(&format!("[OK] Homefiles saved!"));
     }
     // If config has "rootfiles" key, backup every file
     if let Some(files) = config.rootfiles {
         for f in files {
             // WIP
             green(&format!("[SAVE] Copying file {}", f));
-            // Get path to make: 
-            // Example: 
+            // Get path to make:
+            // Example:
             // $DOTFILES_ROOT/etc/portage/repos.conf
-            let tomake = get_to_make(f.path)?;
+            // let tomake = get_to_make(f.path)?;
         }
     }
     Ok(())
