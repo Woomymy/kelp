65a3d18b14d20f9e7c60a8bacc2c1cc620c07da8
Woomymy <github@woomy.ovh>
2021-02-09 11:54:16 +0100

    🔥 Remove shadow_rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-#[macro_use]
-extern crate shadow_rs;
-shadow!(build);
 use structopt::StructOpt;
 
 #[macro_use]
@@ -9,10 +6,7 @@ extern crate kelpdot_macros;
 mod lib;
 use lib::{cli::opts::Cli, terminal::messages::not_yet_implemented};
 fn main() -> anyhow::Result<()> {
-    green!("KelpDot V{}", build::PKG_VERSION);
-    debug_print!("Commit {} Branch {}", build::SHORT_COMMIT, build::BRANCH);
-    debug_print!("Built with {}-{}", build::RUST_VERSION, build::RUST_CHANNEL);
-    debug_print!("Commit by {}", build::COMMIT_AUTHOR);
+    green!("KelpDot V{}", env!("CARGO_PKG_VERSION"));
     green!("==============");
     // Check CLI options
     match Cli::from_args() {
