23588c1f41b99e8aa30491f8c82c5c17a6f1d26e
Woomy <woomy@woomy.be>
2021-02-09 12:42:55 +0100

    🔥 Remove useless deps

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -2,5 +2,4 @@ pub mod cli;
 pub mod config;
 pub mod fsutil;
 pub mod structs;
-pub mod terminal;
 pub mod util;
--- src/lib/terminal/messages.rs
+++
@@ -1,4 +0,0 @@
-/// Say a fonction isn't actually implemented
-pub fn not_yet_implemented() {
-    println!("{}", console::style("Not yet implemented").yellow().bold());
-}
