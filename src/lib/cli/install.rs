bbee6609e06b9271c582ea09f278d987bbfc0ea9
Woomymy <github@woomy.ovh>
2021-02-16 12:05:03 +0100

    ✨ Add rootfiles install

--- src/lib/cli/install.rs
+++ src/lib/cli/install.rs
@@ -1,6 +1,6 @@
-use std::path::Path;
+use std::{path::Path,process::Command};
 use kelpdot_macros::*;
-use crate::lib::{fsutil::{paths::get_root,copy::copy},util::os::get_host_os,config::loader::load_cfg,structs::config::KelpDotConfig};
+use crate::lib::{fsutil::{paths::get_root,copy::copy},util::{os::get_host_os,exec::get_root_exec_program},config::loader::load_cfg,structs::config::KelpDotConfig};
 pub fn install() -> anyhow::Result<()> {
     let root = get_root()?;
     cyan!("[INFO] Installing dotfiles {}", root);
@@ -18,6 +18,29 @@ pub fn install() -> anyhow::Result<()> {
                 copy(format!("{}/{}", home_files_path, file.path), format!("{}/{}", home, file.path))?;
             }
         }
+    }
+    // The work of rootfiles copy is **really** different:
+    // Firstly we check if file exist
+    // We create a Shell script with required files copies
+    // We execute it as root
+    // DONE!
+    if let Some(files) = config.rootfiles {
+        for file in files {
+            let fpath = format!("{}{}", root, file.path);
+            let mut bash_code = String::from("#!/usr/bin/env bash\n#This script has been auto-generated and will be runned by KelpDot\n#It isn't intended to be modified manually\n");
+            let path = Path::new(&fpath);
+            let dest_parent = Path::new(&file.path).parent().unwrap().to_str().unwrap();
+            if path.exists() {
+                bash_code = format!("{}if [[ ! -d {} ]]\nthen\nmkdir -p {}\nfi\ncp -r {} {}\n", bash_code, dest_parent, dest_parent, path.to_str().unwrap(), file.path);
+            }
+            std::fs::write("/tmp/kelpdot_install.sh", bash_code)?;
+        }
+        let rexec = get_root_exec_program()?;
+        Command::new(&rexec) // Use SH because some systems symlinks it to bash / zsh / ash
+                        .arg("sh")
+                        .arg("/tmp/kelpdot_install.sh")
+                        .status()?;
+        
     }
     Ok(())
 }
