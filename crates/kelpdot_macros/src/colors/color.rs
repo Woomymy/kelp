943cd200efe656912e2670e17f4495d6160b04db
Woomymy <github@woomy.ovh>
2021-02-16 13:11:32 +0100

    ⚡️Use ANSI escapes instead of console

--- crates/kelpdot_macros/src/colors/color.rs
+++ crates/kelpdot_macros/src/colors/color.rs
@@ -6,27 +6,27 @@ pub extern crate console;
 /// Prints a text in cyan
 macro_rules! cyan {
     ($($arg:tt)*) => {
-        println!("{}", ::kelpdot_macros::colors::console::style(format!($($arg)*)).cyan().bold());
+        println!("{}", format!("\x1B[1;96m{}\x1b[0;m", format!($($arg)*)));
     };
 }
 #[macro_export]
 /// Prints a text in red
 macro_rules! red {
     ($($arg:tt)*) => {
-        println!("{}", ::kelpdot_macros::colors::console::style(format!($($arg)*)).red().bold());
+        println!("{}", format!("\x1B[1;91m{}\x1b[0;m", format!($($arg)*)));
     };
 }
 #[macro_export]
 /// Prints a text in green
 macro_rules! green {
     ($($arg:tt)*) => {
-        println!("{}", ::kelpdot_macros::colors::console::style(format!($($arg)*)).green().bold());
+        println!("{}", format!("\x1B[1;92m{}\x1b[0;m", format!($($arg)*)));
     };
 }
 #[macro_export]
 /// Prints a text in magenta
 macro_rules! magenta {
     ($($arg:tt)*) => {
-        println!("{}", ::kelpdot_macros::colors::console::style(format!($($arg)*)).magenta().bold());
+        println!("{}", format!("\x1B[1;95m{}\x1b[0;m", format!($($arg)*)));
     };
 }
