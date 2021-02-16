6832a681fa859aaca28de60b4155a0684ee9c649
Woomymy <github@woomy.ovh>
2021-02-16 11:15:33 +0100

    ✨ Install homefiles

--- src/lib/cli/mod.rs
+++ src/lib/cli/mod.rs
@@ -2,3 +2,4 @@ pub mod init;
 pub mod migrate;
 pub mod opts;
 pub mod save;
+pub mod install;
--- src/lib/fsutil/copy.rs
+++ src/lib/fsutil/copy.rs
@@ -1,6 +1,13 @@
+use kelpdot_macros::*;
 use std::path::Path;
 /// Copy a file / Directory
 pub fn copy(source: String, dest: String) -> anyhow::Result<()> {
+    let destpath = Path::new(&dest);
+    let parent = destpath.parent().unwrap();
+    debug_print!("Checking parents path...");
+    if !parent.exists() {
+        std::fs::create_dir_all(parent)?;
+    }
     if Path::new(&source).is_file() {
         std::fs::copy(source, dest)?;
     } else {
