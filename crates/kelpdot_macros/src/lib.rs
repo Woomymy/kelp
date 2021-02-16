8f19861faa3027c90498f38303eae70ddef03a61
Woomy <woomy@woomy.be>
2021-02-16 12:12:16 +0100

    ♻️ Cargo clippy checks and format

--- crates/kelpdot_macros/src/lib.rs
+++ crates/kelpdot_macros/src/lib.rs
@@ -3,10 +3,10 @@ pub mod debug;
 #[test]
 pub fn isdebug_true_works() {
     std::env::set_var("KELP_DEBUG", "true");
-    assert_eq!(true, debug::debug::is_debug())
+    assert_eq!(true, debug::dbg::is_debug())
 }
 #[test]
 pub fn isdebug_false_works() {
     std::env::set_var("KELP_DEBUG", "false");
-    assert_eq!(false, debug::debug::is_debug())
+    assert_eq!(false, debug::dbg::is_debug())
 }
