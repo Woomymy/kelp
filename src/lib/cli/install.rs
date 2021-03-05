ae1396991a97213b8412f29aa722023a17cdec4d
Woomymy <github@woomy.ovh>
2021-03-05 21:07:44 +0100

    Format: Reformat code

--- src/lib/cli/install.rs
+++ src/lib/cli/install.rs
@@ -2,7 +2,7 @@ use crate::lib::{
     config::loader::load_cfg,
     fsutil::{copy::copy, paths::get_root},
     structs::config::KelpDotConfig,
-    util::{exec::get_root_exec_program, scripts::run_script, os::get_host_os},
+    util::{exec::get_root_exec_program, os::get_host_os, scripts::run_script},
 };
 use kelpdot_macros::*;
 use std::{path::Path, process::Command};
@@ -15,7 +15,7 @@ pub fn install() -> anyhow::Result<()> {
     let config: KelpDotConfig = load_cfg(root.clone())?;
     if let Some(scripts) = config.prerun {
         for script in scripts {
-            cyan!("[PRERUN] Running script {}",script.path);
+            cyan!("[PRERUN] Running script {}", script.path);
             run_script(root.clone(), script)?;
         }
     }
@@ -77,7 +77,7 @@ pub fn install() -> anyhow::Result<()> {
     }
     if let Some(scripts) = config.postrun {
         for script in scripts {
-            cyan!("[POSTRUN] Running script {}",script.path);
+            cyan!("[POSTRUN] Running script {}", script.path);
             run_script(root.clone(), script)?;
         }
     }
