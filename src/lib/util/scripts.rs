b1007c5ca69179143cc0fb837ad95d83d4c45004
Woomy4680-exe <matzar1808@gmail.com>
2021-03-05 21:07:44 +0100

    Format: Reformat code

--- src/lib/util/scripts.rs
+++ src/lib/util/scripts.rs
@@ -1,5 +1,5 @@
-use crate::lib::util::exec::get_root_exec_program;
 use crate::lib::structs::script::Script;
+use crate::lib::util::exec::get_root_exec_program;
 use kelpdot_macros::debug_print;
 use std::process::Command;
 pub fn run_script(root: String, script: Script) -> anyhow::Result<()> {
@@ -8,15 +8,18 @@ pub fn run_script(root: String, script: Script) -> anyhow::Result<()> {
             debug_print!("Getting elevator for script {}", script);
             let elevator = get_root_exec_program()?;
             Command::new(elevator) // Use SH because some systems symlinks it to bash / zsh / ash
-            .arg("sh")
-            .arg(&format!("{}/{}", root, script.path))
-            .arg(root)
-            .status()?;
+                .arg("sh")
+                .arg(&format!("{}/{}", root, script.path))
+                .arg(root)
+                .status()?;
         } else {
-            run_script(root, Script {
-                path: script.path,
-                elevated: None
-            })?;
+            run_script(
+                root,
+                Script {
+                    path: script.path,
+                    elevated: None,
+                },
+            )?;
         }
     } else {
         Command::new("sh") // Use SH because some systems symlinks it to bash / zsh / ash
