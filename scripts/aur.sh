11a347ac48ffe65b2a2adfda65061281df16e014
Woomymy <github@woomy.ovh>
2021-03-07 15:51:43 +0100

    CI: Add PKGBUILD generation

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
+set -e
+cargo aur
+cat << EOF >> PKGBUILD
+# Maintainer: Woomy4680-exe <github@woomy.ovh>
+pkgname=kelpdot-bin
+pkgver=@VERSION@
+pkgrel=1
+pkgdesc="Simple dotfiles manager"
+url="https://woomy4680-exe.github.io/kelp-docs/"
+license=("MIT")
+arch=("x86_64")
+provides=("kelpdot")
+options=("strip")
+source=("https://github.com/Woomy4680-exe/kelp/releases/download/$pkgver/kelpdot-$pkgver-x86_64.tar.gz")
+sha256sums=("@PKG_SUM@")
 
-echo "WIP"
+package() {
+    install -Dm755 kelpdot -t "$pkgdir/usr/bin/"
+}
+EOF
+VERSION="$(grep -i version Cargo.toml | head -n 1 | awk '{print $3}' | cut -d '"' -f 2)"
+sed -i "s/@PKGSUM@/$(sha256sum kelpdot-*-x86_64.tar.gz | awk '{print $1}')"
+sed -i "s/@VERSION@/${VERSION}/g"
+cat "PKGBUILD"
