83287338f3d5909dc310c7859e8662b18994a0af
Woomy <woomy@woomy.be>
2021-03-08 14:51:55 +0100

    Scripts: Finish changelog gen

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -4,6 +4,7 @@ red() {
 	echo -e "\e[91m$@\e[m"
 }
 main(){
+	declare -A changelog
 	TAG="${1}"
 	if [[ -z "${1}" ]]
 	then
@@ -14,6 +15,7 @@ main(){
 		exit 1
 	}
 	COMMITS="$(git log --pretty --format="%h" ${TAG}..HEAD)"
+	COMMITTYPES=()
 	for COMMIT in $COMMITS
 	do
 		COMMIT_DESC="$(git log --pretty --format="%B" -n 1 ${COMMIT})" # Get the description of the commit (NOTE: the -n1 option is required) 
@@ -23,6 +25,16 @@ main(){
 		fi
 		COMMIT_TYPE="$(echo $COMMIT_DESC | awk -F ': ' '{ print $1 }')"
 		COMMIT_INFO="$(echo $COMMIT_DESC | awk -F ': ' '{ print $2 }')"
+		changelog[$COMMIT_TYPE]+="- ${COMMIT_INFO}\n"
+	done
+	for ctype in "${!changelog[@]}"
+	do
+		OK_COMMITS="${changelog[$ctype]}"
+		echo -e "## ${ctype}\n"
+		for COMMIT in "${OK_COMMITS}"
+		do
+			echo -e "${COMMIT}"
+		done
 	done
 }
 main $@
