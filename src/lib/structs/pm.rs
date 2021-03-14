3aa0ebbab86d9511598cc996da50003f11161c94
Woomy4680-exe <matzar1808@gmail.com>
2021-03-14 16:09:25 +0100

    PackagesManagers: Add ExternalPackageManger trait

--- src/lib/structs/pm.rs
+++ src/lib/structs/pm.rs
@@ -2,4 +2,10 @@ use anyhow::Result;
 /// Base trait for PM integrations
 trait PackageManager {
     fn install_packages(packages: Vec<String>) -> Result<()>;
+    fn install_packages(package: &str) -> Result<()>;
 }
+/// Packages managers such as NPM or cargo
+trait ExternalPackageManager: PackageManger {
+    fn install_on_distro<T: PackageManger>(pm: T) -> Result<()>;
+}
+
