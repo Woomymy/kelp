7f1f2f60c2cd8cb04267296753291df0d2b1e614
Woomy <woomy@woomy.be>
2021-03-14 16:40:18 +0100

    PackageManagers: Add portage install_package function

--- src/lib/packagemangers/portage.rs
+++ src/lib/packagemangers/portage.rs
@@ -1,8 +1,32 @@
-use crate::lib::structs::PackageManager;
+use std::process::Command;
+use anyhow::{Result,Context};
+use crate::lib::{
+    structs::pm::PackageManager,
+    util::exec::get_root_exec_program,
+};
+use kelpdot_macros::red;
+/// The gentoo's package manager
+pub struct Portage {
+    name: String, 
+}
 
-struct Portage {}
-
-impl PackageManger for Portage {
-
+impl PackageManager for Portage {
+    fn install_package(&self, package: &str) -> Result<()> {
+        let executor = get_root_exec_program()?;
+        Command::new(&executor)
+        .arg("emerge")
+        .arg(package)
+        .status()
+        .with_context(|| red!("Unable to install {} with {}", package, self.name))?;
+        Ok(())
+    }
+    fn install_packages(&self, packages: Vec<String>) -> Result<()> {
+        Ok(())
+    }
+    fn new() -> Self {
+        Self {
+            name: String::from("portage")
+        }
+    }
 }
 
