dae406f05f8e836769d2c349729ee328b5b69050
Woomymy <github@woomy.ovh>
2021-03-14 15:48:02 +0100

    README: Add gentoo install instructions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Context;
 
 #[macro_use]
 extern crate kelpdot_macros;
-use clap::{App, SubCommand, crate_authors};
+use clap::{crate_authors, App, SubCommand};
 mod lib;
 fn main() -> anyhow::Result<()> {
     green_print!("KelpDot V{}", env!("CARGO_PKG_VERSION"));
@@ -12,22 +12,22 @@ fn main() -> anyhow::Result<()> {
         .author(crate_authors!())
         .about("A simple dotfiles manager written in Rust")
         .subcommands(vec![
-            SubCommand::with_name("init").about("Setups kelpdot in the current folder or in $DOTFILES_ROOT"),
+            SubCommand::with_name("init")
+                .about("Setups kelpdot in the current folder or in $DOTFILES_ROOT"),
             SubCommand::with_name("save").about("Saves the dotfiles"),
             SubCommand::with_name("install").about("Installs the dotfiles"),
             SubCommand::with_name("migrate").about("Migrates to newer config versions"),
-        ]).get_matches();  
+        ])
+        .get_matches();
     // Check CLI options
     if let Some(_m) = kelp.subcommand_matches("save") {
         lib::cli::save::save().with_context(|| red!("Unable to save the dotfiles!"))?;
     } else if let Some(_m) = kelp.subcommand_matches("install") {
-        lib::cli::install::install()
-                .with_context(|| red!("Unable to install the dotfiles!"))?;
+        lib::cli::install::install().with_context(|| red!("Unable to install the dotfiles!"))?;
     } else if let Some(_m) = kelp.subcommand_matches("init") {
         lib::cli::init::init().with_context(|| red!("Unable to init kelpdot!"))?;
     } else if let Some(_m) = kelp.subcommand_matches("migrate") {
-        lib::cli::migrate::migrate()
-                .with_context(|| red!("Unable to migrate configurations!"))?;
+        lib::cli::migrate::migrate().with_context(|| red!("Unable to migrate configurations!"))?;
     } else {
         println!("{}", kelp.usage());
     }
