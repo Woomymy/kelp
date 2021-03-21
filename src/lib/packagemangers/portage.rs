0f1aaa0685e70e740150bdca9967badf0aed0721
Woomy4680-exe <matzar1808@gmail.com>
2021-03-21 15:48:56 +0100

    PackageManagers: Prevent duplicating packages on portage

--- src/lib/packagemangers/portage.rs
+++ src/lib/packagemangers/portage.rs
@@ -3,32 +3,54 @@ use anyhow::{Context, Result};
 use kelpdot_macros::red;
 use std::process::Command;
 /// The gentoo's package manager
-pub struct Portage {}
+pub struct Portage {
+    systempackages: Vec<String>,
+}
 
 impl PackageManager for Portage {
-    fn install_package(&self, package: &str) -> Result<()> {
-        let executor = get_root_exec_program()?;
-        Command::new(&executor)
-            .arg("emerge")
-            .arg(package)
-            .status()
-            .with_context(|| red!("Unable to install {}!", package))?;
+    fn install_package(&mut self, package: &str) -> Result<()> {
+        if self.systempackages.len() == 0 {
+            let world = std::fs::read_to_string("/var/lib/portage/world")
+                .with_context(|| red!("Can't read WORLD file!"))?;
+            world.lines().into_iter().for_each(|x| {
+                self.systempackages.push(String::from(x));
+            });
+        }
+        if !self.systempackages.iter().any(|x| x == package) {
+            let executor = get_root_exec_program()?;
+            Command::new(&executor)
+                .arg("emerge")
+                .arg(package)
+                .status()
+                .with_context(|| red!("Unable to install {}!", package))?;
+        }
         Ok(())
     }
 
-    fn install_packages(&self, packages: Vec<String>) -> Result<()> {
+    fn install_packages(&mut self, packages: Vec<String>) -> Result<()> {
         if packages.len() > 0 {
+            if self.systempackages.len() == 0 {
+                let world = std::fs::read_to_string("/var/lib/portage/world")
+                    .with_context(|| red!("Can't read WORLD file!"))?;
+                world.lines().into_iter().for_each(|x| {
+                    self.systempackages.push(String::from(x));
+                });
+            }
             let executor = get_root_exec_program()?;
             let mut cmd = Command::new(&executor);
             cmd.arg("emerge");
             for pack in packages {
-                cmd.arg(pack);
+                if !self.systempackages.iter().any(|x| x.to_string() == pack) {
+                    cmd.arg(pack);
+                }
             }
             cmd.status()?;
         }
         Ok(())
     }
     fn new() -> Self {
-        Self {}
+        Self {
+            systempackages: vec![],
+        }
     }
 }
