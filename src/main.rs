a15e497190a2cbac1812b20e74a54fbf66652ef7
Woomy <woomy@woomy.be>
2021-03-26 18:39:14 +0100

    CLI: Use args instead of using clap

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,34 @@
-use anyhow::Context;
-
 #[macro_use]
 extern crate kelpdot_macros;
-use clap::{crate_authors, App, SubCommand};
 mod lib;
 fn main() -> anyhow::Result<()> {
-    green_print!("KelpDot V{}", env!("CARGO_PKG_VERSION"));
-    green_print!("==============");
-    let kelp = App::new("KelpDot")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(crate_authors!())
-        .about("A simple dotfiles manager written in Rust")
-        .subcommands(vec![
-            SubCommand::with_name("init")
-                .about("Setups kelpdot in the current folder or in $DOTFILES_ROOT"),
-            SubCommand::with_name("save").about("Saves the dotfiles"),
-            SubCommand::with_name("install").about("Installs the dotfiles"),
-        ])
-        .get_matches();
-    // Check CLI options
-    if let Some(_m) = kelp.subcommand_matches("save") {
-        lib::cli::save::save().with_context(|| red!("Unable to save the dotfiles!"))?;
-    } else if let Some(_m) = kelp.subcommand_matches("install") {
-        lib::cli::install::install().with_context(|| red!("Unable to install the dotfiles!"))?;
-    } else if let Some(_m) = kelp.subcommand_matches("init") {
-        lib::cli::init::init().with_context(|| red!("Unable to init kelpdot!"))?;
-    } else {
-        println!("{}", kelp.usage());
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        usage(args);
+        std::process::exit(1);
+    }
+    match args[1].as_str() {
+        "install" => {
+            lib::cli::install::install()?;
+        },
+        "init" => {
+            lib::cli::init::init()?;
+        },
+        "save" => {
+            lib::cli::save::save()?; 
+        },
+        _ => {
+            usage(args);
+            std::process::exit(1);
+        }
     }
     Ok(())
+}
+pub fn usage(args: Vec<String>) {
+    magenta_print!("KelpDot V{}", env!("CARGO_PKG_VERSION"));
+    red_print!("Invalid usage! {}", args.join(" "));    
+    green_print!("Options:");
+    cyan_print!("{} save : Save the dotfiles", args[0]);
+    cyan_print!("{} install : Install the dotfiles", args[0]);
+    cyan_print!("{} init : Initialise Kelpdot", args[0]);
 }
