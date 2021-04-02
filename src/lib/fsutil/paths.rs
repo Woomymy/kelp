2f2ef49c3c925de0bba9a331588a47925081fd35
Woomy4680-exe <matzar1808@gmail.com>
2021-04-02 16:23:59 +0200

    Functions: Add get_env_path fun

--- src/lib/fsutil/paths.rs
+++ src/lib/fsutil/paths.rs
@@ -1,6 +1,24 @@
 use anyhow::Context;
 use kelpdot_macros::red;
 use std::path::Path;
+/// Check if a env var is set or return default
+pub fn get_path_from_env<T: ToString>(var: &str, def: T) -> anyhow::Result<String> {
+    let base = std::env::var(var).unwrap_or(def.to_string());
+    if !Path::new(&base).exists() {
+        return Ok(def.to_string());
+    }
+    let full = std::fs::canonicalize(base)?;
+    Ok(full.to_str().unwrap().to_string())
+}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn get_path_fenv_test() {
+        use super::get_path_from_env;
+        println!("{}", std::env::current_dir().unwrap().to_str().unwrap().to_string());
+        assert_eq!(std::env::current_dir().unwrap().to_str().unwrap().to_string(), get_path_from_env("SOME_USELESS_VAR", ".").unwrap())
+    }
+}
 /// Gets the root of dotfiles using DOTFILES_ROOT path or .
 pub fn get_root() -> anyhow::Result<String> {
     let basepath = std::env::var("DOTFILES_ROOT").unwrap_or_else(|_| String::from("."));
