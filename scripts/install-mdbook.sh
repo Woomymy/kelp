1d99fdfb9e63bd154a425d432e00d92da553e12e
Woomymy <github@woomy.ovh>
2021-05-09 14:34:06 +0200

    CI: Fix mdbook install permission problems

--- scripts/install-mdbook.sh
+++ scripts/install-mdbook.sh
@@ -2,7 +2,6 @@
 # Downloads latest bin version of mdbook
 VERSION="0.4.7"
 URL="https://github.com/rust-lang/mdBook/releases/download/v${VERSION}/mdbook-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-echo $USER
 wget "${URL}" -O "/tmp/mdbook.tar.gz"
-tar xf "/tmp/mdbook.tar.gz" -C "/usr/bin"
+sudo tar xf "/tmp/mdbook.tar.gz" -C "/usr/bin"
 
