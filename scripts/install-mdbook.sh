449678d63fc0821c6843ea986f1e8ec5e0c8bdc8
Woomymy <github@woomy.ovh>
2021-05-09 15:16:12 +0200

    CI: Directly uncompress stdout

--- scripts/install-mdbook.sh
+++ scripts/install-mdbook.sh
@@ -2,6 +2,5 @@
 # Downloads latest bin version of mdbook
 VERSION="0.4.7"
 URL="https://github.com/rust-lang/mdBook/releases/download/v${VERSION}/mdbook-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-wget "${URL}" -O "/tmp/mdbook.tar.gz"
-sudo tar xf "/tmp/mdbook.tar.gz" -C "/usr/bin"
+wget "${URL}" -O - | sudo tar -C "/usr/bin" xzvf -
 
