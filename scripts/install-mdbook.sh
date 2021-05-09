a660ae6294990128faefba4fa3519cc1cba52907
Woomymy <github@woomy.ovh>
2021-05-09 14:29:16 +0200

    CI: Download binary version of mdbook

---
+++ scripts/install-mdbook.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Downloads latest bin version of mdbook
+VERSION="0.4.7"
+URL="https://github.com/rust-lang/mdBook/releases/download/v${VERSION}/mdbook-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+
+wget "${URL}" -O "/tmp/mdbook.tar.gz"
+tar xf "/tmp/mdbook.tar.gz" -C "/usr/local/bin/"
+
