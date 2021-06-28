b758b519725954c81e186b1160c8fed6fca48056
Woomy <woomy@woomy.be>
2021-06-28 16:39:41 +0200

    CI: Try to fix permissions

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -2,4 +2,4 @@
 ls -R ./target
 sudo chown $USER:$USER ./target/release/rpmbuild/RPMS/x86_64/*
 sudo chmod 755 ./target/release/rpmbuild/RPMS/x86_64/*
-mv ./target/release/rpmbuild/RPMS/x86_64/* ./kelpdot.rpm
+sudo mv ./target/release/rpmbuild/RPMS/x86_64/kelpdot-*.rpm ./kelpdot.rpm
