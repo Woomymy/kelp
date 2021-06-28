79153752469971baa8434ea9357bf03a93660a8a
Woomymy <github@woomy.ovh>
2021-06-28 16:39:41 +0200

    CI: Try to fix permissions

--- scripts/deb.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cargo install cargo-deb
-cargo deb
-
-VERSION="$(grep -i version Cargo.toml | head -n 1 | awk '{print $3}' | cut -d '"' -f 2)"
-mv target/debian/*.deb "kelpdot.deb"
