bfc13c31848a097ffe8abe7f8c84af9af3d229e7
Woomy <woomy@woomy.be>
2021-06-28 16:13:44 +0200

    CI: Try to fix CI

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
-mv target/x86_64-unknown-linux-musl/release/rpmbuild/RPMS/x86_64/* kelpdot.rpm
+ls -R ./target
+mv ./target/x86_64-unknown-linux-musl/release/rpmbuild/RPMS/x86_64/* kelpdot.rpm
