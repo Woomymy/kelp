f7069c4dca15510138005affca7291ae7bf4834b
Woomy <woomy@woomy.be>
2021-06-28 16:30:23 +0200

    CI: Fix perms

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 ls -R ./target
-mv ./target/release/rpmbuild/RPMS/x86_64/* kelpdot.rpm
+sudo chown $USER:$USER ./target/release/rpmbuild/RPMS/x86_64/*
+sudo chmod 755 ./target/release/rpmbuild/RPMS/x86_64/*
+mv ./target/release/rpmbuild/RPMS/x86_64/* ./kelpdot.rpm
