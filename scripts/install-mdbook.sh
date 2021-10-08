a40ecb9f47897699bc8c49a5c0eea9ad4cd84439
Woomy <woomy@woomy.be>
2021-10-08 15:19:47 +0200

    CI: Update mdbook

--- scripts/install-mdbook.sh
+++ scripts/install-mdbook.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Downloads latest bin version of mdbook
-VERSION="0.4.10"
+VERSION="0.4.13"
 URL="https://github.com/rust-lang/mdBook/releases/download/v${VERSION}/mdbook-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 wget "${URL}" -O "/tmp/mdbook.tar.gz"
 sudo tar xf "/tmp/mdbook.tar.gz" -C "/usr/bin"
